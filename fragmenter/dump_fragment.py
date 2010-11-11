4cc26fc0eaf4fa81e743f5fe9a1e01917f6e9b89
jk <jk@inventos.ru>
2010-11-11 19:06:51 +0300

    Изменение в скрипте, дополнительный вывод.

--- fragmenter/dump_fragment.py
+++ fragmenter/dump_fragment.py
@@ -3,23 +3,31 @@ import sys
 from struct import unpack
 
 fd = open(sys.argv[1])
+offset = 0
 data = fd.read()
 tagsize = unpack(">I", data[:4])[0]
 assert len(data) == tagsize, "len=%d, tagsize=%d" % (len(data), tagsize)
 assert data[4:8] == "mdat", data[4:8]
-data = data[8:]
+
+def advance(nbytes):
+    global data, offset
+    data = data[nbytes:]
+    offset += nbytes
+
+advance(8)
 while data:
+    print "%08x" % offset, 
     tag = ord(data[0])
     size = unpack(">I", chr(0) + data[1:4])[0]
     timestamp = unpack(">I", data[7] + data[4:7])[0]
     if tag == 9:
-        print "video, ",
+        print "video, composition offset = %s" % (unpack(">I", chr(0) + data[13:16])[0],),
     elif tag == 8:
         print "audio, ",
     else:
         print "tag=%d, " % tag,
     print "size=%d, timestamp=%d" % (size, timestamp)
     total = size + 11
-    data = data[total:]
+    advance(total)
     assert unpack(">I", data[:4])[0] == total
-    data = data[4:]
+    advance(4)
