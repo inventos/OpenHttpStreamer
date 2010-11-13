e9daa3a6601a116ffe9b519e8a2bcf10907d9e05
jk <jk@local.(none)>
2010-11-13 16:37:18 +0300

    Удалён ненужный код.

--- fragmenter/base64.cc
+++ fragmenter/base64.cc
@@ -30,7 +30,7 @@ namespace base64 {
     void encode(std::streambuf *buf, const char *bytes, unsigned size) {
         unsigned rest = size % 3;
         const char *limit = bytes + size - rest;
-        for ( const char *ptr = bytes; ptr < limit; ptr += 3, size -= 3 ) {
+        for ( const char *ptr = bytes; ptr < limit; ptr += 3 ) {
             buf->sputc(letters[ (ptr[0] >> 2) & 0x3F ]);
             buf->sputc(letters[ ((ptr[0] << 4) & 0x30) | ((ptr[1] >> 4) & 0xF) ]);
             buf->sputc(letters[ ((ptr[1] << 2) & 0x3C) | ((ptr[2] >> 6) & 0x3) ]);
