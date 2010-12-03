302449833b06a57e9d82f5a73bfabf330c7e61d4
jk <e.karpachov@gmail.com>
2010-12-03 16:08:14 +0300

    Copyright notice added.

--- mp4frag/mapping.hh
+++ mp4frag/mapping.hh
@@ -1,3 +1,23 @@
+/*
+ * 
+ * copyright (c) 2010 ZAO Inventos (inventos.ru)
+ * copyright (c) 2010 jk@inventos.ru
+ * copyright (c) 2010 kuzalex@inventos.ru
+ * copyright (c) 2010 artint@inventos.ru
+ *
+ * This file is part of mp4frag.
+ *
+ * mp4grag is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mp4frag is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
 #ifndef __mapping_hh__739ccb21_1898_4774_9e40_797f62fbbdaf
 #define __mapping_hh__739ccb21_1898_4774_9e40_797f62fbbdaf
 
