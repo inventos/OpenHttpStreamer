cb8881470de0387abc98c1e121bb70c9a5f8de70
jk <e.karpachov@gmail.com>
2010-12-16 18:13:57 +0300

    Copyright notice added.

--- mp4frag/mp4frag.hh
+++ mp4frag/mp4frag.hh
@@ -1,3 +1,23 @@
+/*
+ * 
+ * copyright (c) 2010 ZAO Inventos (inventos.ru)
+ * copyright (c) 2010 jk@inventos.ru
+ * copyright (c) 2010 kuzalex@inventos.ru
+ * copyright (c) 2010 artint@inventos.ru
+ *
+ * This file is part of mp4frag.
+ *
+ * mp4grag is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mp4frag is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
 #ifndef __mp4frag_hh__ac9d6258_0bd9_4b2f_b934_984389f48934
 #define __mp4frag_hh__ac9d6258_0bd9_4b2f_b934_984389f48934
 
